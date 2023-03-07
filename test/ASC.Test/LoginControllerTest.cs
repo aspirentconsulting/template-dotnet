dfa6a7b5e1bc41918b9df57a303ae5b957689b77
islands04 <sgarcia04@hotmail.com>
2023-03-07 18:25:13 -0500

    added unit test for LoginController

---
+++ test/ASC.Test/LoginControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using ASC.API.Controllers;
+using ASC.API.Request;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ASC.Test
+{
+    [TestClass]
+    public class LoginControllerTest
+    {
+        private Mock<ILogger<LoginController>> moqLogger = new Mock<ILogger<LoginController>>();
+
+        [TestMethod]
+        public void TestGreetUserByPostInvalidUser()
+        {
+            // var moqLogger = new Mock<ILogger<LoginController>>();
+            // moqLogger.Setup(x => x.LogDebug(It.IsAny<string>()));
+            var loginController = new LoginController(moqLogger.Object);
+            loginController.ModelState.AddModelError("Name", "Input is required");
+            var response = loginController.GreetUserByPost(new User(string.Empty));
+            Assert.IsInstanceOfType<BadRequestObjectResult>(response);
+        }
+
+        [TestMethod]
+        public void TestGreetUserByPostValidUser()
+        {
+            var loginController = new LoginController(moqLogger.Object);
+
+            // loginController.ModelState.AddModelError("Name", "Input is required");
+            var response = loginController.GreetUserByPost(new User("Sam"));
+            Assert.IsInstanceOfType<CreatedResult>(response);
+        }
+    }
+}
